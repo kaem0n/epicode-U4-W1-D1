e988b7a987d91f9eb4f95eb351888f46331134d4
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-03-04 15:42:14 +0100

    finished exercise 2

---
+++ src/Exercise2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci tre parole");
+        String word1 = scanner.nextLine();
+        System.out.println("Prima parola inserita: " + word1);
+        String word2 = scanner.nextLine();
+        System.out.println("Seconda parola inserita: " + word2);
+        String word3 = scanner.nextLine();
+        System.out.println("Terza parola inserita: " + word3);
+        System.out.println("Concatenazione delle parole: " + word1 + " " + word2 + " " + word3);
+        System.out.println("Concatenazione delle parole (inverso): " + word3 + " "+ word2 + " " + word1);
+    }
+}
